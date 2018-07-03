9f3642035067eb9a346c17d2262981f5124cc959
candyder3te <39513905+candyder3te@users.noreply.github.com>
2018-07-03 22:00:47 +0200

    Add files via upload

--- js/devicemotion5.js
+++ js/devicemotion5.js
@@ -1,6 +1,9 @@
 if (window.DeviceMotionEvent) {
     document.getElementById("dmeSupported").innerText = "OK - Device Motion wird unterstützt!";
     window.addEventListener('devicemotion', function(event) {
+            var alpha;
+            var beta;
+            var gamma;
             document.getElementById("alphaRotation").innerHTML = 'alpha ' + event.rotationRate.alpha.toFixed(2);
             document.getElementById("betaRotation").innerHTML = 'beta ' + event.rotationRate.beta.toFixed(2);
             document.getElementById("gammaRotation").innerHTML = 'gamma ' + event.rotationRate.gamma.toFixed(2);
