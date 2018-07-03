5077f8ff6b8a1dcbaf73fe9a3adfcc515203bc6e
candyder3te <39513905+candyder3te@users.noreply.github.com>
2018-07-03 22:07:12 +0200

    Add files via upload

---
+++ js/devicemotion6.js
@@ -0,0 +1,35 @@
+if (window.DeviceOrientationEvent) {
+    window.addEventListener("deviceorientation", function(event) {
+        // alpha: rotation around z-axis
+        var rotateDegrees = event.alpha;
+        // gamma: left to right
+        var leftToRight = event.gamma;
+        // beta: front back motion
+        var frontToBack = event.beta;
+
+        handleOrientationEvent(frontToBack, leftToRight, rotateDegrees);
+    }, true);
+}
+
+var handleOrientationEvent = function(frontToBack, leftToRight, rotateDegrees) {
+    // do something amazing
+};
+
+
+
+function showError(error) {
+    switch (error.code) {
+        case error.PERMISSION_DENIED:
+            alert('Abfrage der Geoposition untersagt.');
+            break;
+        case error.POSITION_UNAVAILABLE:
+            alert('Es sind keine Geopositionsdaten verfügbar.');
+            break;
+        case error.TIMEOUT:
+            alert('Timeout überschritten.');
+            break;
+        default:
+            alert('Unbekannter Fehler');
+            break;
+    }
+}
