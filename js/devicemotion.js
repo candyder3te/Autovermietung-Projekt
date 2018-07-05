51964196b9c50570064406884f19a7e152505f1f
candyder3te <39513905+candyder3te@users.noreply.github.com>
2018-07-05 08:14:06 +0200

    Add files via upload

--- js/devicemotion.js
+++ js/devicemotion.js
@@ -2,9 +2,9 @@ if (window.DeviceMotionEvent) {
     document.getElementById("dmeSupported").innerText = "OK - Device Motion wird unterstützt!";
     window.addEventListener('devicemotion', function(event) {
 
-        document.getElementById("xRotation").innerHTML = 'Rotationsrate beta um x-Achse: ' + event.rotationRate.beta.toFixed(2);
-        document.getElementById("yRotation").innerHTML = 'Rotationsrate gamma um y-Achse: ' + event.rotationRate.gamma.toFixed(2);
-        document.getElementById("zRotation").innerHTML = 'Rotationsrate alpha um z-Achse: ' + event.rotationRate.alpha.toFixed(2);
+        document.getElementById("xRotation").innerHTML = 'Rotation um die x-Achse: ' + event.rotationRate.beta.toFixed(2);
+        document.getElementById("yRotation").innerHTML = 'Rotation um die y-Achse: ' + event.rotationRate.gamma.toFixed(2);
+        document.getElementById("zRotation").innerHTML = 'Rotation um die z-Achse: ' + event.rotationRate.alpha.toFixed(2);
 
     });
 } else {
